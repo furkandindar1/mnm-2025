2472dd35f442ea70b9a0d0377db970ba5050c476
furkandindar1 <furkan@mnm.com.tr>
2025-06-18 16:38:39 +0300

    Update hero.js

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,3 +1,12 @@
 export default function decorate(block) {
-  block.classList.add('hero');
+  const rows = [...block.children];
+  const content = document.createElement('div');
+  content.className = 'hero-content';
+
+  rows.forEach((row) => {
+    content.appendChild(row);
+  });
+
+  block.innerHTML = '';
+  block.appendChild(content);
 }
